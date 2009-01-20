05b9468c17bb2ccd308888e658903947e85ce172
nunb <nunb@fuseki.org>
2009-01-20 01:45:34 +0100

    fixed a broken build: damn quote characters! Stx should warn you in the first para, in BOLD!

--- build-ppcre-core.lisp
+++ build-ppcre-core.lisp
@@ -1,5 +1,5 @@
 (with-output-to-string (*standard-output*)
-  (push #p"./clbuild/systems/" asdf:*central-registry*)
+  (push #p"/Users/Nandan/nb/clbuild/systems/" asdf:*central-registry*)
   (require :cl-ppcre))
 
 (save-lisp-and-die "cl-ppcre.sbcl" :executable t)
