4223642afba4169b9dc8f4a80fcc38b0fdbfae16
Leslie P. Polzer <polzer@gnu.org>
2009-05-31 18:39:01 +0200

    amended line parser program

--- syntax.lisp
+++ syntax.lisp
@@ -5,21 +5,43 @@
 
 ;(trace read-line)
 
+(defmacro push-end (value place &environment env)
+  "Like PUSH, except that the value goes on the end of the PLACE list.  
+If PLACE is (), then (value) is returned."
+  #+Explorer (declare (ignore env))
+  (multiple-value-bind (tmpvars tmpvals storevar storeform refform)
+      (get-setf-expansion place #-Explorer env)
+    (let ((oldval  (gensym "OLDVAL"))
+          (newcons (gensym "NEWCONS")))
+    `(let* (,@(mapcar #'list tmpvars tmpvals)
+            (,oldval ,refform)
+            (,newcons (cons ,value nil)))
+       (if ,oldval
+           (progn (setf (cdr (last ,oldval)) ,newcons)
+                  ,oldval)
+           (let ((,@storevar ,newcons))
+             ,storeform))))))
+
 (defun filter (&optional (in *standard-input*) (out *standard-output*))
   (loop for line = (read-line in nil)
         while line
         ;do (format t "line: ~S~%" line)
         do (if (cl-ppcre:all-matches "^CODE_BEGIN.*$" line)
-              (let* ((lines (loop for line = (read-line in)
+              (let* ((newline-string (format nil "~%"))
+                     (lines (loop for line = (read-line in)
                                   while line
                                   until (cl-ppcre:all-matches "^CODE_END" line)
-                                  collect (append (coerce line 'list) '(#\Newline))))
+                                  collect (concatenate 'string line newline-string)))
                      (lines (apply #'concatenate 'string lines))) ;; FIXME reduce
+                (format out "{{{~%")
+                (finish-output out)
                 (run-program "/usr/bin/pygmentize" '("-lcl" "-fhtml")
                              :input (make-string-input-stream lines)
-                             :output out))
-            (progn
-              (write-sequence line out)
-              (terpri out)))))
+                             :output out)
+                (write "}}}" :stream out :escape nil))
+              (progn
+                (write-sequence line out)
+                (terpri out)))))
 
 (filter *standard-input* *standard-output*)
+
